7a6bcfaf5852d7c0e408449d8eaf450c3e8db64b
suhyun-kim9 <ksh020674@gmail.com>
2025-05-21 15:39:59 +0900

    Navigating/Prefetching/API-Routes

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,11 +1,20 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { NextPage } from "next";
+import { ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <header>글로벌 헤더</header>
-      <Component {...pageProps} />
-    </>
-  );
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: NextPageWithLayout;
+}) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,3 +1,19 @@
+// CSS Module
+// Next.js에서는 페이지별로 css의 클래스명이 겹치는 문제를 원천 차단하기 위해 사용
+import SearchableLayout from "@/components/searchable-layout";
+import style from "./index.module.css";
+import { ReactNode } from "react";
+
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
+  );
 }
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
+
