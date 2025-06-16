a118eae98176e0a2aba1132552855b0db296e911
suhyun-kim9 <ksh020674@gmail.com>
2025-06-16 13:56:47 +0900

    Style추가

---
+++ section03/legacy_src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,13 @@
+import ClientComponent from "@/components/client-component";
+
+export default async function Page({ searchParams }: { searchParams: Promise<{ q: string }> }) {
+  const { q } = await searchParams;
+  return (
+    <div>
+      Search Page: {q}
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  );
+}
---
+++ section03/legacy_src/app/book/[id]/page.tsx
@@ -0,0 +1,13 @@
+import ClientComponent from "@/components/client-component";
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  return (
+    <div>
+      book/{id} page입니다.
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  );
+}
