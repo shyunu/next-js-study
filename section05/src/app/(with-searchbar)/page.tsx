262d37aff8c9b57f3a1fe8488ce00b3175ed74ed
suhyun-kim9 <ksh020674@gmail.com>
2025-06-25 22:09:09 +0900

    Route Segments

--- section05/src/app/(with-searchbar)/page.tsx
+++ section05/src/app/(with-searchbar)/page.tsx
@@ -3,6 +3,13 @@ import style from "./page.module.css";
 import books from "@/mock/books.json";
 import { BookData } from "@/types";
 
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정하는 옵션
+// 1. auto : 기본값으로, 아무것도 강제하지 않는다.
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static : 페이지를 강제로 Static 페이지로 설정
+// 4. error : 페이지를 강제로 Static 페이지 설정 (설정하면 안되는 이유가 있다면 빌드 오류 발생)
+// export const dynamic = "auto";
+
 async function AllBooks() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, { cache: "force-cache" });
   if (!response.ok) {
