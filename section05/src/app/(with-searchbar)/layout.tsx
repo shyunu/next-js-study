2a318907ae6fed66e88792578d5f896abd80327d
suhyun-kim9 <ksh020674@gmail.com>
2025-06-26 14:32:56 +0900

    Client Router Cache

--- section05/src/app/(with-searchbar)/layout.tsx
+++ section05/src/app/(with-searchbar)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode, Suspense } from "react";
 import Searchbar from "../../components/searchbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (  
+  return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
         <Searchbar />
