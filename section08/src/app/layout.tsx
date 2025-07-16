42602b1e190e2cb648356f35407982eae84e6be6
suhyun-kim9 <ksh020674@gmail.com>
2025-07-16 21:16:31 +0900

    Parallel Route

---
+++ section08/src/app/layout.tsx
@@ -0,0 +1,42 @@
+import "./globals.css";
+import Link from "next/link";
+import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, { cache: "force-cache" });
+
+  if (!response.ok) {
+    return <footer>제작 @shyunu</footer>;
+  }
+
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>제작 @shyunu</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  );
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <div className={style.container}>
+          <header>
+            <Link href={"/"}>📚 SHYUNU&apos;S BOOKS</Link>
+          </header>
+          <main>{children}</main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
